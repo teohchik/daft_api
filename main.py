be2ece4b465d8b93e1f82d04adf7b976e0dc8959
teosha <dima.trotsiuk322@gmail.com>
2025-10-18 15:02:37 +0100

    switch port to 8005

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ app.include_router(cookies.router, tags=["cookies"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8005)
